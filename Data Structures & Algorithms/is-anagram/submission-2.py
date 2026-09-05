c371e223a30192eeb1e1d0e238ee6a5f0cd7698f
Seow Jie Ning <seowjiening03@gmail.com>
2026-09-05 11:53:13 +0800

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        countS = {}
+        countT = {}
+
+        for i in s:
+            if i in countS:
+                countS[i] +=1
+            else:
+                countS[i] = 1
+        
+        for i in t:
+            if i in countT:
+                countT[i] +=1
+            else:
+                countT[i] = 1
+
+        return countS == countT
